69b606c96a074f73b64e5d89e21c19ae8a193f16
sweak <hubert.tacik@proton.me>
2025-02-22 11:35:02 +0100

    Updated GitHubTrendsIcons

--- app/src/main/java/com/sweak/githubtrends/core/designsystem/icon/GitHubTrendsIcons.kt
+++ app/src/main/java/com/sweak/githubtrends/core/designsystem/icon/GitHubTrendsIcons.kt
@@ -1,12 +1,10 @@
 package com.sweak.githubtrends.core.designsystem.icon
 
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.TrendingUp
 import androidx.compose.material.icons.automirrored.outlined.ArrowBackIos
 import androidx.compose.material.icons.filled.DarkMode
 import androidx.compose.material.icons.filled.LightMode
 import androidx.compose.material.icons.outlined.ErrorOutline
-import androidx.compose.material.icons.outlined.Star
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.vectorResource
@@ -15,9 +13,12 @@ import com.sweak.githubtrends.R
 object GitHubTrendsIcons {
     val GitHub @Composable get() = ImageVector.vectorResource(R.drawable.ic_github)
     val Fork @Composable get() = ImageVector.vectorResource(R.drawable.ic_fork)
+    val Star @Composable get() = ImageVector.vectorResource(R.drawable.ic_star)
+    val Growth @Composable get() = ImageVector.vectorResource(R.drawable.ic_trend)
+    val Issue @Composable get() = ImageVector.vectorResource(R.drawable.ic_issue)
+    val License @Composable get() = ImageVector.vectorResource(R.drawable.ic_license)
+    val Watch @Composable get() = ImageVector.vectorResource(R.drawable.ic_watch)
 
-    val Star = Icons.Outlined.Star
-    val Growth = Icons.AutoMirrored.Filled.TrendingUp
     val BackArrow = Icons.AutoMirrored.Outlined.ArrowBackIos
     val Error = Icons.Outlined.ErrorOutline
     val LightMode = Icons.Filled.LightMode
